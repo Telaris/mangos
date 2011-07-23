995bd5a4adcc65e9df42e512e74c85a7424e1738
fallenangelX <iceis2@hotmail.com>
2011-07-23 00:36:18 -0400

    fixes for bots movements and ect by blueboy

--- src/game/playerbot/PlayerbotAI.cpp
+++ src/game/playerbot/PlayerbotAI.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2011 MangosR2
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -1889,13 +1890,7 @@ void PlayerbotAI::DoCombatMovement()
 
     float targetDist = m_bot->GetDistance(m_targetCombat);
 
-    // if m_bot has it's back to the attacker, turn
-    if (!m_bot->HasInArc(M_PI_F, m_targetCombat))
-    {
-        // TellMaster("%s is facing the wrong way!", m_bot->GetName());
-        m_bot->GetMotionMaster()->Clear(true);
-        m_bot->SetOrientation(m_bot->GetAngle(m_targetCombat));
-    }
+    m_bot->SetFacingTo(m_bot->GetAngle(m_targetCombat));
 
     if (m_combatStyle == COMBAT_MELEE && !m_bot->hasUnitState(UNIT_STAT_CHASE) && ((m_movementOrder == MOVEMENT_STAY && targetDist <= ATTACK_DISTANCE) || (m_movementOrder != MOVEMENT_STAY)))
         // melee combat - chase target if in range or if we are not forced to stay
@@ -2779,20 +2774,6 @@ void PlayerbotAI::UpdateAI(const uint32 p_time)
 {
     if (m_bot->IsBeingTeleported() || m_bot->GetTrader())
         return;
-
-    // Send updates to world if chasing target or moving to point
-    MovementGeneratorType movementType = m_bot->GetMotionMaster()->GetCurrentMovementGeneratorType();
-    if (movementType == CHASE_MOTION_TYPE || movementType == POINT_MOTION_TYPE)
-    {
-        float x, y, z;
-        if (m_bot->GetMotionMaster()->GetDestination(x, y, z))
-        {
-            m_bot->MonsterMoveWithSpeed(x, y, z, DEFAULT_WALK_SPEED);
-            m_destX = x;
-            m_destY = y;
-            m_destZ = z;
-        }
-    }
 
     time_t currentTime = time(0);
     if (currentTime < m_ignoreAIUpdatesUntilTime)
--- src/game/playerbot/PlayerbotRogueAI.cpp
+++ src/game/playerbot/PlayerbotRogueAI.cpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2005-2011 MaNGOS <http://getmangos.com/>
+ * Copyright (C) 2011 MangosR2
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
